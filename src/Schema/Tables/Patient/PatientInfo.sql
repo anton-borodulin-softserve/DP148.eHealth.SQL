59ae1bbb894918caf65c41b0e09650b9b3eb2198
FSuhov <aksu@ukr.net>
2018-11-06 19:23:50 +0200

    PatientInfo and Images tables added.

---
+++ src/Schema/Tables/Patient/PatientInfo.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[PatientInfo]
+(
+	[PatientId] INT NOT NULL  IDENTITY(1,1), 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Country] VARCHAR(50) NULL, 
+    [City] VARCHAR(50) NULL, 
+    [Address] VARCHAR(MAX) NULL, 
+    [BirthDate] DATE NOT NULL, 
+    [Phone] VARCHAR(13) NULL, 
+    [Genre] NCHAR(10) NULL,
+	[Email] VARCHAR(50) NULL,
+	[ImageId] INT NULL
+	CONSTRAINT PK_Patient PRIMARY KEY CLUSTERED ( [PatientId] )
+	CONSTRAINT UC_Person UNIQUE(FirstName, LastName, BirthDate)    
+	CONSTRAINT FK_Images_Id FOREIGN KEY ( [ImageId] ) REFERENCES dbo.Images( [ImageId] )
+    
+)
