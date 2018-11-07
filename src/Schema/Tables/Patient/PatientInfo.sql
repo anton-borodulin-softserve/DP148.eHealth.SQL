52735641ef496476eef72855b1be355ddd50d649
FSuhov <aksu@ukr.net>
2018-11-07 10:50:56 +0200

    Anastasia's review fix. Some syntaxis mistakes fixed.

--- src/Schema/Tables/Patient/PatientInfo.sql
+++ src/Schema/Tables/Patient/PatientInfo.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[PatientInfo]
 (
-	[PatientId] INT NOT NULL  IDENTITY(1,1), 
+	[PatientId] INT NOT NULL IDENTITY(1, 1), 
     [FirstName] VARCHAR(50) NOT NULL, 
     [LastName] VARCHAR(50) NOT NULL, 
     [Country] VARCHAR(50) NULL, 
@@ -8,11 +8,12 @@
     [Address] VARCHAR(MAX) NULL, 
     [BirthDate] DATE NOT NULL, 
     [Phone] VARCHAR(13) NULL, 
-    [Genre] NCHAR(10) NULL,
+    [Gender] VARCHAR(6) NULL,
 	[Email] VARCHAR(50) NULL,
 	[ImageId] INT NULL
-	CONSTRAINT PK_Patient PRIMARY KEY CLUSTERED ( [PatientId] )
+
+	CONSTRAINT PK_PatientInfo PRIMARY KEY CLUSTERED ( [PatientId] )
 	CONSTRAINT UC_Person UNIQUE(FirstName, LastName, BirthDate)    
-	CONSTRAINT FK_Images_Id FOREIGN KEY ( [ImageId] ) REFERENCES dbo.Images( [ImageId] )
-    
+	CONSTRAINT FK_Images_Id FOREIGN KEY ([ImageId]) REFERENCES dbo.Images([ImageId])
+	CONSTRAINT GenderEnum CHECK (Gender IN ('Male', 'Female'))
 )
