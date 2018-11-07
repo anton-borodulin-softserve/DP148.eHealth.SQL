bb97f8465fcee9fe4e79eafdebbab138dc1a18e2
AntonBorodulin <aborod@softserveinc.com>
2018-11-07 14:00:53 +0200

    Fix Roles FK constraint

--- src/Schema/Tables/UsersLogin.sql
+++ src/Schema/Tables/UsersLogin.sql
@@ -4,5 +4,6 @@
     [Login] VARCHAR(100) NOT NULL,
     [RoleId] INT NOT NULL,
 
-    CONSTRAINT PK_UserLogin PRIMARY KEY CLUSTERED ( [Id] )
+    CONSTRAINT PK_UserLogin PRIMARY KEY CLUSTERED ( [Id] ),
+    CONSTRAINT FK_UsersLogin_Roles FOREIGN KEY ( [RoleId] ) REFERENCES [dbo].[Roles]( [RoleId] )
 )
