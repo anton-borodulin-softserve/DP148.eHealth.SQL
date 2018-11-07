106e146dd4633841e1693c17ad994a4a4f80de36
FSuhov <aksu@ukr.net>
2018-11-07 18:18:49 +0200

    Text fields changed to NVARCHAR. Soft delete added. Email is Unique now. Added script for test data.

--- src/Schema/StaticData/static_UserLogin.sql
+++ src/Schema/StaticData/static_UserLogin.sql
@@ -26,3 +26,33 @@ IF ( NOT EXISTS(SELECT *
 GO
 
 --TODO: Add secret etc.
+
+/* 
+ Post-Deployment Script For inserting Test Image.
+ This file contains SQL statements that will be appended to the build script.
+*/
+
+IF (NOT EXISTS(SELECT *
+			   FROM [dbo].[Images]))
+  BEGIN
+      INSERT INTO [dbo].[Images]
+				  (ImageName)
+	  VALUES      ('TestImageName1')
+  END
+
+GO
+
+/* 
+ Post-Deployment Script For inserting Test Patient.
+ This file contains SQL statements that will be appended to the build script.
+*/
+
+IF (NOT EXISTS(SELECT *
+			   FROM [dbo].[PatientInfo]))
+  BEGIN
+      INSERT INTO [dbo].[PatientInfo]
+				  (FirstName, LastName, Country, City, [Address], BirthDate, Phone, Gender, Email, ImageId, IsDeleted)
+	  VALUES      ('FirstName1', 'LastName1', 'Country1', 'City1', 'Test Street, 101', '2000-01-01', '+380673222233', 1, 'testemail@mail.com', 1, 0)
+  END
+
+GO
