36ed73d5d7b109c70bfb65eb0d663accfb8b8ddf
MaksimTsedov <maksim.czedov.99@gmail.com>
2018-11-08 22:09:50 +0200

    Fixed merge problem

--- src/Schema/Scripts/PostDeployment.sql
+++ src/Schema/Scripts/PostDeployment.sql
@@ -6,7 +6,3 @@
 
 :r ..\StaticData\static_Roles.sql
 :r ..\StaticData\static_UsersLogin.sql
-:r ..\StaticData\static_Allergies.sql
-:r ..\StaticData\static_Symptoms.sql
-:r ..\StaticData\static_PatientAllergy.sql
-:r ..\StaticData\static_AllergySymptom.sql
