7f13a9ead1c674c2ba9daa8ac93b35b684d2a4b4
FSuhov <aksu@ukr.net>
2018-11-08 11:38:50 +0200

    Anton's comments fix

--- src/Schema/Tables/Patient/PatientInfo.sql
+++ src/Schema/Tables/Patient/PatientInfo.sql
@@ -2,19 +2,19 @@
 (
 	[PatientId] INT NOT NULL IDENTITY(1, 1), 
 	[FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Country] NVARCHAR(50) NULL, 
-    [City] NVARCHAR(50) NULL, 
-    [Address] NVARCHAR(MAX) NULL, 
-    [BirthDate] DATE NOT NULL, 
-    [Phone] VARCHAR(13) NULL, 
-    [Gender] BIT NULL,
+	[LastName] NVARCHAR(50) NOT NULL, 
+	[Country] NVARCHAR(50) NULL, 
+	[City] NVARCHAR(50) NULL, 
+	[Address] NVARCHAR(MAX) NULL, 
+	[BirthDate] DATE NOT NULL, 
+	[Phone] VARCHAR(13) NULL, 
+	[Gender] BIT NULL,
 	[Email] VARCHAR(50) NULL,
 	[ImageId] INT NULL,
 	[IsDeleted] BIT NOT NULL DEFAULT 0
 
 	CONSTRAINT PK_PatientInfo PRIMARY KEY CLUSTERED ( [PatientId] )
-	CONSTRAINT UC_Person UNIQUE(FirstName, LastName, BirthDate)
-	CONSTRAINT UC_Email UNIQUE(email)
-	CONSTRAINT FK_Images_Id FOREIGN KEY ([ImageId]) REFERENCES dbo.Images([ImageId])
+	CONSTRAINT UC_Person UNIQUE( [FirstName], [LastName], [BirthDate] )
+	CONSTRAINT UC_Email UNIQUE( [Email] )
+	CONSTRAINT FK_Images_Id FOREIGN KEY ( [ImageId] ) REFERENCES dbo.Images( [ImageId] )
 )
