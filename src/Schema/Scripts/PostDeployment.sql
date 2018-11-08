d182bb6f2fdd959059e9620792c1bec2e1ffa5f2
AntonBorodulin <aborod@softserveinc.com>
2018-11-08 10:26:29 +0200

    Split initial static data script on two separate files

--- src/Schema/Scripts/PostDeployment.sql
+++ src/Schema/Scripts/PostDeployment.sql
@@ -1,4 +1,8 @@
-﻿--Post-Deployment Script base endpoint
---------------------------------------------------------------------------------------
+﻿/* 
+ Post-Deployment Script base endpoint.
+ This file contains SQL statements that will be appended to the build script.
+ Scripts will run according to th order in this file!
+*/
 
-:r ..\StaticData\static_UserLogin.sql
+:r ..\StaticData\static_Roles.sql
+:r ..\StaticData\static_UsersLogin.sql
