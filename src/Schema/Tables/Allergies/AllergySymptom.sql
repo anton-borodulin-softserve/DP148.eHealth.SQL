86e0f718c4252841302acc8e76f76d77685d9442
MaksimTsedov <maksim.czedov.99@gmail.com>
2018-11-08 22:30:15 +0200

    Added tables and sql scripts to test.

---
+++ src/Schema/Tables/Allergies/AllergySymptom.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[AllergySymptom] 
+(
+    [AllergySymptomId] INT NOT NULL IDENTITY, 
+    [PatientAllergyId] INT NOT NULL, 
+    [SymptomId] INT NOT NULL, 
+    [IsDeleted] BIT NOT NULL, 
+
+    CONSTRAINT PK_AllergySymptom PRIMARY KEY ( [AllergySymptomId] ), 
+    CONSTRAINT FK_AllergySymptom_PatientAllergy FOREIGN KEY ( [PatientAllergyId] ) REFERENCES PatientAllergy( [PatientAllergyId] ), 
+    CONSTRAINT FK_AllergySymptom_Symptom FOREIGN KEY ( [SymptomId] ) REFERENCES Symptoms( [Id] ), 
+    CONSTRAINT UC_AllergySymptom UNIQUE ( [PatientAllergyId], [SymptomId] ), 
+)
