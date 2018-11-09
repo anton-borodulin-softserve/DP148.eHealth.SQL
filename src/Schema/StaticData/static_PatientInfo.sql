e297fb1e5e0387ff994aafa37e3fe8664881a029
FSuhov <aksu@ukr.net>
2018-11-09 12:06:03 +0200

    Test data fixed

--- src/Schema/StaticData/static_PatientInfo.sql
+++ src/Schema/StaticData/static_PatientInfo.sql
@@ -5,7 +5,7 @@ IF (NOT EXISTS(SELECT *
   BEGIN
       INSERT INTO [dbo].[PatientInfo]
 				  (FirstName, LastName, Country, City, [Address], BirthDate, Phone, Gender, Email, ImageId, IsDeleted)
-	  VALUES      ('FirstName1', 'LastName1', 'Country1', 'City1', 'Test Street, 101', '2000-01-01', '+380673222233', 1, 'testemail@mail.com', 1, 0)
+	  VALUES      ('FirstName1', 'LastName1', 'Country1', 'City1', 'Test Street, 101', '2000-01-01', '380673222233', 1, 'testemail@mail.com', 1, 0)
   END
 
 GO
