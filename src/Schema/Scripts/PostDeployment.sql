dc0eddb8f10a71b55551dd63ac79da8874705a84
Julia Kropivnaya <kropisha@gmail.com>
2018-11-11 00:21:21 +0200

    Removed garbage

--- src/Schema/Scripts/PostDeployment.sql
+++ src/Schema/Scripts/PostDeployment.sql
@@ -9,15 +9,3 @@
 :r ..\StaticData\static_Secrets.sql
 :r ..\StaticData\static_UsersData.sql
 :r ..\StaticData\static_Sessions.sql
-:r ..\StaticData\static_UsersLogin.sql
-:r ..\StaticData\static_Images.sql
-:r ..\StaticData\static_PatientInfo.sql
-:r ..\StaticData\static_Allergies.sql
-:r ..\StaticData\static_Symptoms.sql
-:r ..\StaticData\static_PatientAllergy.sql
-:r ..\StaticData\static_AllergySymptom.sql
-:r ..\StaticData\static_Medications.sql
-:r ..\StaticData\static_PatientMediacations.sql
-:r ..\StaticData\static_DiseaseCategories.sql
-:r ..\StaticData\static_Diseases.sql
-:r ..\StaticData\static_PatientDiseases.sql
--- src/Schema/Tables/Deseases/DiseaseCategories.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[DiseaseCategories]
-(
-	[CategoryId] INT NOT NULL IDENTITY(1, 1),
-	[Name] NVARCHAR(100) NOT NULL,
-	[IsDeleted] BIT NOT NULL DEFAULT 0,
-
-	CONSTRAINT PK_DiseaseCategories PRIMARY KEY CLUSTERED ( [CategoryId] ),
-	CONSTRAINT UC_CategoryName UNIQUE ( [Name] )
-)
