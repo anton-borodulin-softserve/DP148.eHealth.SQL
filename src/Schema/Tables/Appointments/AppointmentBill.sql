e0dc668434ee10142c13d998f690eb3069133da0
Denis Abramenkov <40663161+DenyaAbramenkov@users.noreply.github.com>
2018-11-10 12:37:12 +0200

    Fixes

--- src/Schema/Tables/Appointments/AppointmentBill.sql
+++ src/Schema/Tables/Appointments/AppointmentBill.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[AppointmentBill] 
   ( 
-     [AppointmentBillId] INT NOT NULL IDENTITY(1, 1), 
-     [InvoiceNumber]     UNIQUEIDENTIFIER NOT NULL, 
-     [Amount]            SMALLMONEY NOT NULL,          
+     [AppointmentId]	 INT NOT NULL, 
+     [InvoiceNumber]     UNIQUEIDENTIFIER NOT NULL DEFAULT ( NEWID() ), 
+     [Amount]            SMALLMONEY NOT NULL,   
+	 [Completed]		 BIT NOT NULL DEFAULT 0,
      [IsDeleted]         BIT NOT NULL DEFAULT 0 
-
-     CONSTRAINT PK_Bills PRIMARY KEY CLUSTERED ( [AppointmentBillId] ), 
+	 
+	 CONSTRAINT FK_AppointmentBill_Appointment FOREIGN KEY ([AppointmentId]) REFERENCES [Appointments]([AppointmentId]),
+     CONSTRAINT PK_Bills PRIMARY KEY CLUSTERED ( [AppointmentId] ), 
      CONSTRAINT UC_InvoiceNumber UNIQUE ( [InvoiceNumber] ), 
-     CONSTRAINT CHK_Amount CHECK ( [Amount] >= 0 ) 
+     CONSTRAINT CHK_Amount CHECK ( [Amount] >= 0 ), 
+    
 	 )
--- src/Schema/Tables/Appointments/Appointments.sql
+++ src/Schema/Tables/Appointments/Appointments.sql
@@ -3,19 +3,14 @@
      [AppointmentId]     INT NOT NULL IDENTITY(1, 1), 
      [PatientId]         INT NOT NULL, 
      [UserId]            INT NOT NULL, 
-     [AppointmentDate]   DATE NOT NULL DEFAULT Getdate(), 
+     [AppointmentDate]   DATE NOT NULL DEFAULT GETDATE(), 
      [AppointmentTime]   TIME NOT NULL, 
      [Duration]          INT NOT NULL DEFAULT 15, 
      [Purpose]           NVARCHAR(50) NOT NULL DEFAULT ('inspection'), 
-     [AppointmentBillId] INT NOT NULL, 
+	 [IsDeleted]         BIT NOT NULL DEFAULT 0
 
-     CONSTRAINT PK_Appointment PRIMARY KEY CLUSTERED ( [AppointmentId] ), 
-     CONSTRAINT FK_Appointments_PatientInfo FOREIGN KEY ( [PatientId] ) 
-     REFERENCES [PatientInfo]( [PatientId] ), 
-     CONSTRAINT FK_Appointments_DoctorView FOREIGN KEY ( [UserId] ) REFERENCES 
-     [DoctorView]( [UserId] ), 
-     CONSTRAINT FK_Appointments_AppointmentBill FOREIGN KEY ( 
-     [AppointmentBillId]) REFERENCES [AppointmentBill]([AppointmentBillId]), 
+     CONSTRAINT PK_Appointments PRIMARY KEY CLUSTERED ( [AppointmentId] ), 
+     CONSTRAINT FK_Appointments_PatientInfo FOREIGN KEY ( [PatientId] ) REFERENCES [PatientInfo]( [PatientId] ), 
      CONSTRAINT CHK_DURATION CHECK ( [Duration] >= 5 ), 
      CONSTRAINT CHK_PURPOSE CHECK ([Purpose] IN('inspection', 'procedure')), 
   )
