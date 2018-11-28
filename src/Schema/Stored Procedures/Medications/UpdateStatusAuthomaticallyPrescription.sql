b9243c81539a9de12b53926b0cd74aa1d4dfe6e4
smoukiDev <smakfactory@gmail.com>
2018-11-29 01:09:23 +0200

    Added two stored procedures for Prescriptions table.

---
+++ src/Schema/Stored Procedures/Medications/UpdateStatusAuthomaticallyPrescription.sql
@@ -0,0 +1,40 @@
+﻿-- Summary:
+-- Goes through all records which were not been deleted
+-- and updates status of prescriptions to historic
+-- operating current date, assignment date and duration.
+CREATE PROCEDURE [dbo].[UpdateStatusAuthomaticallyPrescription]
+AS
+BEGIN
+    DECLARE @Id INT;
+    DECLARE @AssignmentDate DATE;
+    DECLARE @Duration SMALLINT;
+    -- @CurrentDuration of prescription
+    -- Forms as result current date - assignment date
+    -- GETDATE()  - AssignmentDate
+    DECLARE @CurrentDuration INT;
+    DECLARE @PrescriptionCursor CURSOR;
+
+    SET @PrescriptionCursor = CURSOR FOR
+    SELECT [Id], [AssignmentDate], [Duration]
+    FROM [dbo].[Prescriptions] 
+    WHERE [IsFinished] = 0 AND [IsDeleted] = 0;
+
+    OPEN @PrescriptionCursor
+
+    FETCH NEXT FROM @PrescriptionCursor
+    INTO @Id, @AssignmentDate, @Duration
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @CurrentDuration = DATEDIFF(day, @AssignmentDate, GETDATE());
+        IF (@CurrentDuration > @Duration)
+            BEGIN
+                UPDATE [dbo].[Prescriptions] SET [IsFinished] = 1 WHERE Id = @Id;
+            END;
+        FETCH NEXT FROM @PrescriptionCursor
+        INTO @Id, @AssignmentDate, @Duration
+    END;
+    CLOSE @PrescriptionCursor;
+    DEALLOCATE @PrescriptionCursor;
+END
+GO
---
+++ src/Schema/Stored Procedures/Medications/UpdateStatusManuallyPrescription.sql
@@ -0,0 +1,33 @@
+﻿-- Summary:
+-- Change prescription record specified by identifier, setting status to historic.
+-- Operates current date, assignment date and duration.
+-- If current duration more then assignment duration, sets status to historic and leave duration unchanged.
+-- If current duration less then assignment duration, sets status to historic and changes duration up-to-date.
+-- Parameter @Id:
+-- Prescription identifier to make changes.
+CREATE PROCEDURE [dbo].[UpdateStatusManuallyPrescription] @Id INT
+AS
+BEGIN
+    DECLARE @Duration SMALLINT;
+    -- @CurrentDuration of prescription
+    -- Forms as result current date - assignment date
+    -- GETDATE()  - AssignmentDate
+    DECLARE @CurrentDuration INT;
+
+    SELECT @Duration = [Duration], @CurrentDuration = DATEDIFF(day, [AssignmentDate], GETDATE())
+    FROM [dbo].[Prescriptions] WHERE [Id] = @Id AND [IsDeleted] = 0;
+
+    IF (@CurrentDuration > @Duration)
+        BEGIN
+            UPDATE [dbo].[Prescriptions]
+            SET [IsFinished] = 1
+            WHERE [Id] = @Id;
+        END;
+    ELSE
+        BEGIN
+            UPDATE [dbo].[Prescriptions]
+            SET [Duration] = @CurrentDuration, [IsFinished] = 1
+            WHERE [Id] = @Id;
+        END;
+END
+GO
